333a1f9125ab9d5654d861fc15138899cccb77dd
celinamax <celinamax01@gmail.com>
2018-10-23 21:13:04 -0200

    correção Array de Restaurantes

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -2,7 +2,7 @@ export interface Restaurant {
 
   id: string
   name: string
-  categoria: string
+  category: string
   deliveryEstimate: string
   rating: number
   imagePath: string
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {Restaurant} from './restaurant/restaurant.model';
 
@@ -8,23 +8,23 @@ import {Restaurant} from './restaurant/restaurant.model';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurant: Restaurant[] = [
+  restaurants: Restaurant[] = [
     {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
+      id: 'bread-bakery',
+      name: 'Bread & Bakery',
+      category: 'Bakery',
+      deliveryEstimate: '25m',
       rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png",
+      imagePath: 'assets/img/restaurants/breadbakery.png'
 
     },
     {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
+      id: 'burger-house',
+      name: 'Burger House',
+      category: 'Hamburgers',
+      deliveryEstimate: '100m',
       rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png",
+      imagePath: 'assets/img/restaurants/burgerhouse.png'
 
     }
   ]
