d764d69d5f27e9821c1aa2fa0430cbcb4ad2041a
celinamax <celinamax01@gmail.com>
2018-10-24 16:53:15 -0200

    Criando a classe serviço para Restaurantes

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Restaurant } from './restaurant.model';
 
 @Component({
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,37 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Restaurant} from './restaurant/restaurant.model';
+import{Restaurant} from './restaurant/restaurant.model';
+import{RestaurantsService} from './restaurant/restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html'
-})
-export class RestaurantsComponent implements OnInit {
+  })
+  export class RestaurantsComponent implements OnInit{
 
-  restaurants: Restaurant[] = [
-    {
-      id: 'bread-bakery',
-      name: 'Bread & Bakery',
-      category: 'Bakery',
-      deliveryEstimate: '25m',
-      rating: 4.9,
-      imagePath: 'assets/img/restaurants/breadbakery.png'
+    restaurants: Restaurant[]
 
-    },
-    {
-      id: 'burger-house',
-      name: 'Burger House',
-      category: 'Hamburgers',
-      deliveryEstimate: '100m',
-      rating: 3.5,
-      imagePath: 'assets/img/restaurants/burgerhouse.png'
+    constructor(private restaurantsService: RestaurantsService) {}
+
+    ngOnInit() {
+      this.restaurants = this.restaurantsService.restaurants() 
+      /*A variável restaurants está recebendo a variável restaurantsService 
+      com o método restaurants do componente restaurants.component? */
 
     }
-  ]
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
-}
